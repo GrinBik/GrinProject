36a4bf889ce30da1663aa55d3c954b41d9c86f37
Gregorii Bikineev <gregorybik@gmail.com>
2022-05-23 16:38:35 +0200

    Create programm for starting CMD.

---
+++ cmd_start.py
@@ -0,0 +1,6 @@
+# Импортирую модуль системы для запуска необходимых программ
+import os
+
+# Запускаем CMD с необходимой директорией
+os.chdir("C:/Users/user/AppData/Local/Programs/Python/Python310/Scripts")
+os.startfile("C:/Windows/System32/cmd.exe")
